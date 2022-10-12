124b2be91578a8c63efc11ea32acc0704b630c3f
Maxim Topciu <mtopciu@adguard.com>
2022-10-12 18:22:16 +0300

    fix app path determination

--- ElectronMainApp/src/utils/app-pack.js
+++ ElectronMainApp/src/utils/app-pack.js
@@ -1,8 +1,18 @@
+const path = require('path');
+const { app, ipcRenderer } = require('electron');
+
 module.exports = (() => {
     const _resourcePath = function (resPath) {
-        const path = require('path');
-        // TODO check if this would work in production
-        return path.join(process.cwd(), resPath);
+        let base;
+        if (app) {
+            base = app.getAppPath();
+        } else {
+            base = ipcRenderer.sendSync('renderer-to-main', JSON.stringify({
+                'type': 'getAppPath',
+            }));
+        }
+
+        return path.join(base, resPath);
     };
 
     return {

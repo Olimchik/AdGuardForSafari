429bb5306d4f996f0e2d13ac093bc6e6557cd30e
Maxim Topciu <mtopciu@adguard.com>
2022-10-13 08:19:31 +0300

    document resourcePath module work

--- ElectronMainApp/src/utils/app-pack.js
+++ ElectronMainApp/src/utils/app-pack.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const { app, ipcRenderer } = require('electron');
 
+/**
+ * This module can be used in both main and renderer process
+ * If it is used in the main process then app.getAppPath() is used
+ * If it is used in the renderer process then ipcRenderer sends message to main process to get app path
+ */
 module.exports = (() => {
     const _resourcePath = function (resPath) {
         let base;

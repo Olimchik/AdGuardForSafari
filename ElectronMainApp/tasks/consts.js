f07f82d2c05c2acd7f560db2a7c6a639172ca4ec
tvinzz <o.labudko@adguard.com>
2022-10-22 00:20:16 +0700

    updated twosky link

--- ElectronMainApp/tasks/consts.js
+++ ElectronMainApp/tasks/consts.js
@@ -2,4 +2,4 @@ module.exports.LOCALES_DIR = 'locales';
 
 module.exports.PRIVATE_FILES = '../private';
 
-module.exports.TRANSLATION_SERVICE_URL = 'https://twosky.adtidy.org/api/v1/';
+module.exports.TRANSLATION_SERVICE_URL = 'https://twosky.int.agrd.dev/api/v1/';
